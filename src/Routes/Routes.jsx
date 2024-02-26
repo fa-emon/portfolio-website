cc33d7f8a912df06dec06de54d96940c08da7308
Farhan933 <farhanfarabiemon@gmail.com>
2024-02-26 13:20:26 +0600

    adding hero-section

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
+import Home from "../components/Home/Home";
 
 export const router = createBrowserRouter([
     {
@@ -7,7 +8,8 @@ export const router = createBrowserRouter([
         element: <Main></Main>,
         children: [
             {
-                path: '/'
+                path: '/',
+                element: <Home></Home>
             }
         ]
     }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import HeroSection from "../HeroSection/HeroSection";
+
+
+const Home = () => {
+    return (
+        <div>
+            <HeroSection></HeroSection>
+        </div>
+    );
+};
+
+export default Home;
