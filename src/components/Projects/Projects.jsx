997a4b46ae0e3d959399859aa9c3a319c6b422a8
Farhan933 <farhanfarabiemon@gmail.com>
2024-02-28 15:15:36 +0600

    adding workflow

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -26,29 +26,31 @@ const Projects = () => {
     }, [])
 
     return (
-        <div className="mt-32">
-            <h2 className='text-2xl primary-font ms-24' style={{ background: 'linear-gradient(to left, rgba(255, 255, 255, 1), rgba(169, 169, 169, 0.5))', color: 'transparent', display: 'inline', WebkitBackgroundClip: 'text', backgroundClip: 'text' }}>PROJECTS</h2>
+        <>
+            <div className="mt-32">
+                <h2 className='text-2xl primary-font ms-24' style={{ background: 'linear-gradient(to left, rgba(255, 255, 255, 1), rgba(169, 169, 169, 0.5))', color: 'transparent', display: 'inline', WebkitBackgroundClip: 'text', backgroundClip: 'text' }}>PROJECTS</h2>
 
-            {
-                loading ? <>
-                    <span className="loading loading-ring loading-xs"></span>
-                    <span className="loading loading-ring loading-sm"></span>
-                    <span className="loading loading-ring loading-md"></span>
-                    <span className="loading loading-ring loading-lg"></span>
-                </>
-                    :
-                    <>
-                        <div className="grid md:grid-cols-3 lg:grid-cols-3 gap-10 px-20 mt-6">
-                            {
-                                projects.map(project => <ShowProjects
-                                    key={project._id}
-                                    project={project}
-                                ></ShowProjects>)
-                            }
-                        </div>
+                {
+                    loading ? <>
+                        <span className="loading loading-ring loading-xs"></span>
+                        <span className="loading loading-ring loading-sm"></span>
+                        <span className="loading loading-ring loading-md"></span>
+                        <span className="loading loading-ring loading-lg"></span>
                     </>
-            }
-        </div>
+                        :
+                        <>
+                            <div className="grid md:grid-cols-3 lg:grid-cols-3 gap-10 px-20 mt-6">
+                                {
+                                    projects.map(project => <ShowProjects
+                                        key={project._id}
+                                        project={project}
+                                    ></ShowProjects>)
+                                }
+                            </div>
+                        </>
+                }
+            </div>
+        </>
     );
 };
 
--- src/components/Projects/ShowProjects.jsx
+++ src/components/Projects/ShowProjects.jsx
@@ -1,25 +1,55 @@
 import { Link } from "react-router-dom";
 
-
 const ShowProjects = ({ project }) => {
-    const { title, image, description, client_side, server_side, github_client, github_server, skills } = project;
+    const { title, image, description, client_side, server_side, github_client, github_server, workflow, skills } = project;
 
     return (
         <div className="card w-96 bg-black shadow-xl">
-            <figure><img className="transform hover:scale-90 transition-transform duration-700 hover:brightness-95 hover:rounded-2xl" src={image} alt="project" style={{ height: '256px', objectFit: 'cover', width: '100%' }} /></figure>
+            <figure>
+                <img className="transform hover:scale-90 transition-transform duration-700 hover:brightness-95 hover:rounded-2xl" src={image} alt="project" style={{ height: '256px', objectFit: 'cover', width: '100%' }} />
+            </figure>
             <div className="card-body">
                 <h2 className="card-title text-white primary-font">{title}</h2>
                 <p className="text-[#808080] secondary-font">{description}</p>
-                <div className="flex justify-between">
-                    <button className="btn btn-sm border-none bg-[#090e13] text-white hover:bg-[#808080] hover:text-black primary-font px-[17px]"><Link to={client_side}>Live Link Client</Link></button>
 
-                    <button className="btn btn-sm border-none bg-[#090e13] text-white hover:bg-[#808080] hover:text-black primary-font px-[17px]"><Link to={server_side}>Live Link Server</Link></button>
-                </div>
-                <div className="flex justify-between">
-                    <button className="btn btn-sm border-none bg-[#090e13] text-white hover:bg-[#808080] hover:text-black primary-font"><Link to={github_client}>Client Side Code</Link></button>
+                {(client_side || server_side || github_client || github_server) && (
+                    <>
+                        <div className="flex justify-between">
+                            {client_side && (
+                                <button className="btn btn-sm border-none bg-[#090e13] text-white hover:bg-[#808080] hover:text-black primary-font px-[17px]">
+                                    <Link to={client_side}>Live Link Client</Link>
+                                </button>
+                            )}
 
-                    <button className="btn btn-sm border-none bg-[#090e13] text-white hover:bg-[#808080] hover:text-black primary-font"><Link to={github_server}>Server Side Code</Link></button>
-                </div>
+                            {server_side && (
+                                <button className="btn btn-sm border-none bg-[#090e13] text-white hover:bg-[#808080] hover:text-black primary-font px-[17px]">
+                                    <Link to={server_side}>Live Link Server</Link>
+                                </button>
+                            )}
+                        </div>
+                        <div className="flex justify-between">
+                            {github_client && (
+                                <button className="btn btn-sm border-none bg-[#090e13] text-white hover:bg-[#808080] hover:text-black primary-font">
+                                    <Link to={github_client}>Client Side Code</Link>
+                                </button>
+                            )}
+
+                            {github_server && (
+                                <button className="btn btn-sm border-none bg-[#090e13] text-white hover:bg-[#808080] hover:text-black primary-font">
+                                    <Link to={github_server}>Server Side Code</Link>
+                                </button>
+                            )}
+                        </div>
+                    </>
+                )}
+
+                {!client_side && !server_side && !github_client && !github_server && workflow && (
+                    <div>
+                        <button className="btn btn-sm border-none bg-[#090e13] text-white hover:bg-[#808080] hover:text-black primary-font w-full mt-4">
+                            <Link to={workflow}>Live Link</Link>
+                        </button>
+                    </div>
+                )}
             </div>
         </div>
     );
